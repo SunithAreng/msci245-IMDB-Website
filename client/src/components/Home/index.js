b8940999426f36dd2af865a8cb853af92d2edf2c
SunithAreng <sareng@uwaterloo.ca>
2022-06-16 02:57:58 -0400

    ll

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,172 +1,236 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+import { createTheme, ThemeProvider, styled } from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
 
 
-//Dev mode
-//const serverURL = ""; //enable for dev mode
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 
-//Deployment mode instructions
-const serverURL = "http://ov-research-4.uwaterloo.ca:3032"; //enable for deployed mode; Change PORT to the port number given to you;
-//To find your port number: 
-//ssh to ov-research-4.uwaterloo.ca and run the following command: 
-//env | grep "PORT"
-//copy the number only and paste it in the serverURL in place of PORT, e.g.: const serverURL = "http://ov-research-4.uwaterloo.ca:3000";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Box from "@material-ui/core/Box";
 
-const fetch = require("node-fetch");
+
+const serverURL = "http://ov-research-4.uwaterloo.ca:3032";
 
 const opacityValue = 0.9;
 
-const theme = createTheme({
+
+const lightTheme = createTheme({
   palette: {
-    type: 'dark',
+    type: 'light',
     background: {
-      default: "#000000"
+      default: "#ffffff"
     },
     primary: {
-      main: "#52f1ff",
+      main: '#ef9a9a',
+      light: '#ffcccb',
+      dark: '#ba6b6c',
+      background: '#eeeeee'
     },
     secondary: {
-      main: "#b552f7",
+      main: "#b71c1c",
+      light: '#f05545',
+      dark: '#7f0000'
     },
   },
 });
 
-const styles = theme => ({
-  root: {
-    body: {
-      backgroundColor: "#000000",
-      opacity: opacityValue,
-      overflow: "hidden",
-    },
-  },
-  mainMessage: {
-    opacity: opacityValue,
-  },
+const MainGridContainer = styled(Grid)(({ theme }) => ({
+  margin: theme.spacing(4),
+}))
 
-  mainMessageContainer: {
-    marginTop: "20vh",
-    marginLeft: theme.spacing(20),
-    [theme.breakpoints.down('xs')]: {
-      marginLeft: theme.spacing(4),
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    "& > *": {
+      margin: theme.spacing(-1),
+      width: "30ch"
+    },
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '30ch',
     },
   },
   paper: {
-    overflow: "hidden",
+    height: 140,
+    width: 100,
   },
-  message: {
-    opacity: opacityValue,
-    maxWidth: 250,
-    paddingBottom: theme.spacing(2),
+  control: {
+    padding: theme.spacing(3),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
   },
 
-});
+}));
 
+export default function SpacingGrid() {
+  const [spacing, setSpacing] = React.useState(1);
+  const classes = useStyles();
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userID: 1,
-      mode: 0
-    }
+  const handleRatingChange = (event) => {
+    setSpacing(Number(event.target.value));
   };
 
-  componentDidMount() {
-    this.loadUserSettings();
-  }
+  const [movieName, setMovieName] = React.useState([]);
 
+  const handleMovieChange = (event) => {
+    setMovieName(event.target.value);
+  };
 
-  loadUserSettings() {
-    this.callApiLoadUserSettings()
-      .then(res => {
-        //console.log("loadUserSettings returned: ", res)
-        var parsed = JSON.parse(res.express);
-        console.log("loadUserSettings parsed: ", parsed[0].mode)
-        this.setState({ mode: parsed[0].mode });
-      });
-  }
-
-  callApiLoadUserSettings = async () => {
-    const url = serverURL + "/api/loadUserSettings";
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        //authorization: `Bearer ${this.state.token}`
-      },
-      body: JSON.stringify({
-        userID: this.state.userID
-      })
-    });
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    console.log("User settings: ", body);
-    return body;
-  }
-
-  render() {
-    const { classes } = this.props;
-
-
-
-    const mainMessage = (
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        justify="flex-start"
-        alignItems="flex-start"
-        style={{ minHeight: '100vh' }}
-        className={classes.mainMessageContainer}
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Box
+        sx={{
+          height: '100vh',
+          opacity: opacityValue,
+          overflow: "hidden",
+          //backgroundColor: lightTheme.palette.background.default,
+          //backgroundImage: `url(${BackgroundImage})`,
+          //backgroundImage: `url(https://www.treehugger.com/thmb/huWogB9GGnLTMn-MmpMxlWos_BE=/2500x1881/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2019__12__ParadiseExterior-13-c8711698f75e4ca9b8b7673989362a57.jpg)`,
+          backgroundSize: "cover"
+        }}
       >
-        <Grid item>
-
-          <Typography
-            variant={"h3"}
-            className={classes.mainMessage}
-            align="flex-start"
-          >
-            {this.state.mode === 0 ? (
-              <React.Fragment>
-                Finally got access to this webpage!
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                Vamos! Vamos! Vamos!
-              </React.Fragment>
-            )}
+        <MainGridContainer
+          container
+          spacing={1}
+          style={{ maxWidth: '50%' }}
+          direction="column"
+          justify="flex-start"
+          alignItems="stretch"
+        >
+          <Typography variant="h3" component="div">
+            Movie Review
+          </Typography>
+          <Typography variant="h6" component="div">
+            Check out the reviews before you decide to buy the tickets.
           </Typography>
 
-        </Grid>
-      </Grid>
-    )
+          <Grid container>
+            <MovieSelection
+              handleChange={handleMovieChange}
+              classes={classes}
+              movieName={movieName}
+            />
+          </Grid>
 
+          <Grid container>
+            <ReviewTitle
+              classes={classes}
+            />
+          </Grid>
 
-    return (
-      <MuiThemeProvider theme={theme}>
-        <div className={classes.root}>
-          <CssBaseline />
-          <Paper
-            className={classes.paper}
-          >
-            {mainMessage}
-          </Paper>
+          <Grid container>
+            <ReviewBody />
+          </Grid>
 
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+          <Grid container>
+            <ReviewRating
+              spacing={spacing}
+              handleChange={handleRatingChange}
+              movieName={movieName}
+            />
+          </Grid>
+
+          <Grid container>
+            <Button variant="contained" color="secondary">
+              Submit
+            </Button>
+          </Grid>
+
+        </MainGridContainer>
+      </Box>
+    </ThemeProvider>
+
+  );
 }
 
-Home.propTypes = {
-  classes: PropTypes.object.isRequired
-};
+const MovieSelection = ({ handleChange, classes, movieName }) => {
+  return (
+    <>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink htmlFor="moviesList">Select Movies</InputLabel>
+        <Select
+          value={movieName}
+          onChange={handleChange}
+          inputProps={{
+            name: 'movies',
+            id: 'moviesList',
+          }}
+        >
+          <option value="">None</option>
+          <option value={1}>Kingsman</option>
+          <option value={2}>Crazy Rich Asians</option>
+          <option value={3}>Revenant</option>
+          <option value={4}>Thor: Love and Thunder</option>
+          <option value={5}>Doctor Strange: Multiverse in Madness</option>
+        </Select>
+      </FormControl>
+    </>
+  )
+}
 
-export default withStyles(styles)(Home);
+const ReviewRating = ({ spacing, handleChange }) => {
+  return (
+    <>
+      <Grid item>
+        <FormLabel>Rating</FormLabel>
+        <RadioGroup
+          name="Rating"
+          aria-label="Rating"
+          value={spacing.toString()}
+          onChange={handleChange}
+          row
+        >
+          {[1, 2, 3, 4, 5].map((value) => (
+            <FormControlLabel
+              key={value}
+              value={value.toString()}
+              control={<Radio />}
+              label={value.toString()}
+            />
+          ))}
+        </RadioGroup>
+      </Grid>
+    </>
+  )
+}
+
+const ReviewTitle = ({ classes }) => {
+  return (
+    <>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="review-title" label="Review Title" variant="filled" />
+      </form>
+    </>
+  )
+}
+
+const ReviewBody = () => {
+  return (
+    <>
+      <TextField
+        id="outlined-multiline-static"
+        label="Enter a Review"
+        multiline
+        rows={4}
+        defaultValue=""
+        variant="outlined"
+      />
+    </>
+  )
+}
+

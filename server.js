316d0e33214898bbf8f1a64b74cb706f796f3e1c
“SunithAreng” <sareng@uwaterloo.ca>
2022-07-11 18:54:21 -0400

    D2 Done

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-var cors = require('cors');
-app.use(cors());
+// var cors = require('cors');
+// app.use(cors());
 
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// app.use(function (req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+// 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-	if ('OPTIONS' === req.method) {
-		res.send(200);
-	}
-	else {
-		next();
-	}
-});
+// 	if ('OPTIONS' === req.method) {
+// 		res.send(200);
+// 	}
+// 	else {
+// 		next();
+// 	}
+// });
 
 const movies = [
 	{
